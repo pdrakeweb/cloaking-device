b4f58129215c211438288692c02536bc296560d1
Peter Drake <pdrake@allplayers.com>
2012-12-12 16:01:58 -0500

    Adding Cloak and GroupNode classes.

---
+++ src/CloakingDevice/Cloak.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Provides an on-demand object injector.
+ *
+ * @author pdrake
+ *
+ */
+
+Namespace CloakingDevice;
+
+class Cloak extends stdClass {
+
+  /**
+   * Contains the injected object.
+   *
+   * @var stdClass
+   */
+  protected $object;
+
+  /**
+   * Indicates whether the object has been fully loaded.
+   *
+   * @var bool
+   */
+  protected $loaded = FALSE;
+
+
+  /**
+   * Stores NULL in a variable for return by reference.
+   *
+   * @var NULL
+   */
+  public $_null_value = NULL;
+
+  /**
+   * Build the object.
+   */
+  public function __construct() {
+    $this->object = new stdClass();
+  }
+
+  /**
+   * Implementation of the magic __get method.
+   *
+   * @param string $requested_property
+   * @return mixed
+   */
+  public function &__get($requested_property) {
+    // If the property is not set and we aren't fully loaded, attempt to load
+    // the property directly or via node_load.
+    if (!isset($this->object->$requested_property) && !$this->loaded) {
+      $this->load();
+    }
+
+    if (isset($this->object->$requested_property)) {
+      return $this->object->$requested_property;
+    }
+    else {
+      return $this->_null_value;
+    }
+  }
+
+  /**
+   * Implements the magic __set method.
+   *
+   * @param string $set_property
+   * @param mixed $value
+   */
+  public function __set($set_property, $value) {
+    $this->object->$set_property = $value;
+  }
+
+  /**
+   * Implements the magic __isset method.
+   *
+   * @param string $property
+   */
+  public function __isset($property) {
+    return ($this->__get($property) !== NULL);
+  }
+
+  /**
+   * Implements the magic __unset method.
+   *
+   * @param string $property
+   */
+  public function __unset($property) {
+    unset($this->object->$property);
+  }
+
+  /**
+   * Helper function to load the object.
+   */
+  protected function load() {
+    $this->loaded = TRUE;
+  }
+
+}
---
+++ src/CloakingDevice/Drupal/GroupNode.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Provides an on-demand object injector.
+ *
+ * @author pdrake
+ *
+ */
+
+Namespace CloakingDevice\Drupal;
+
+use CloakingDevice\Cloak as Cloak;
+
+class GroupNode extends Cloak {
+
+  /**
+   * Build the node object.
+   *
+   * @param integer $nid
+   */
+  public function __construct($nid) {
+    $this->object = new stdClass();
+    $this->object->nid = $nid;
+  }
+
+  /**
+   * Implementation of the magic __get method.
+   *
+   * @param string $requested_property
+   * @return mixed
+   */
+  public function &__get($requested_property) {
+    // If the property is not set and we aren't fully loaded, attempt to load
+    // the property directly or via node_load.
+    if (!isset($this->object->$requested_property) && !$this->loaded) {
+      switch ($requested_property) {
+        case 'og_private':
+          $sql = 'SELECT og_private FROM {og} WHERE nid = %d';
+          $properties = db_fetch_array(db_query($sql, $this->object->nid));
+          foreach ($properties as $property => $value) {
+            $this->object->$property = $value;
+          }
+          break;
+
+        default:
+          $this->load();
+        break;
+      }
+    }
+
+    if (isset($this->object->$requested_property)) {
+      return $this->object->$requested_property;
+    }
+    else {
+      return $this->_null_value;
+    }
+  }
+
+  /**
+   * Helper function to load the node object.
+   */
+  protected function load() {
+    $this->object = node_load($this->object->nid);
+    $this->loaded = TRUE;
+  }
+
+}
