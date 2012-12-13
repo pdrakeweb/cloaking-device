ff82143143da8b5b0eb4ee6a0cb03f27a969d6b8
Peter Drake <pdrake@allplayers.com>
2012-12-13 09:14:10 -0500

    Convert code style to follow PSR-2 standards.

--- src/CloakingDevice/Cloak.php
+++ src/CloakingDevice/Cloak.php
@@ -1,100 +1,101 @@
 <?php
+namespace CloakingDevice;
+
+use stdClass;
+
 /**
  * Provides an on-demand object injector.
- *
- * @author pdrake
- *
  */
+class Cloak extends stdClass
+{
 
-Namespace CloakingDevice;
+    /**
+     * Contains the injected object.
+     *
+     * @var stdClass
+     */
+    protected $object;
 
-use \stdClass;
-
-class Cloak extends stdClass {
-
-  /**
-   * Contains the injected object.
-   *
-   * @var stdClass
-   */
-  protected $object;
-
-  /**
-   * Indicates whether the object has been fully loaded.
-   *
-   * @var bool
-   */
-  protected $loaded = FALSE;
+    /**
+     * Indicates whether the object has been fully loaded.
+     *
+     * @var bool
+     */
+    protected $loaded = false;
 
 
-  /**
-   * Stores NULL in a variable for return by reference.
-   *
-   * @var NULL
-   */
-  public $_null_value = NULL;
+    /**
+     * Stores NULL in a variable for return by reference.
+     *
+     * @var NULL
+     */
+    public $_null_value = null;
 
-  /**
-   * Build the object.
-   */
-  public function __construct() {
-    $this->object = new stdClass();
-  }
-
-  /**
-   * Implementation of the magic __get method.
-   *
-   * @param string $requested_property
-   * @return mixed
-   */
-  public function &__get($requested_property) {
-    // If the property is not set and we aren't fully loaded, attempt to load
-    // the property directly or via node_load.
-    if (!isset($this->object->$requested_property) && !$this->loaded) {
-      $this->load();
+    /**
+     * Build the object.
+     */
+    public function __construct()
+    {
+          $this->object = new stdClass();
     }
 
-    if (isset($this->object->$requested_property)) {
-      return $this->object->$requested_property;
+    /**
+     * Implementation of the magic __get method.
+     *
+     * @param string $requested_property
+     * @return mixed
+     */
+    public function &__get($requested_property)
+    {
+        // If the property is not set and we aren't fully loaded, attempt to load
+        // the property directly or via node_load.
+        if (!isset($this->object->$requested_property) && !$this->loaded) {
+            $this->load();
+        }
+
+        if (isset($this->object->$requested_property)) {
+            return $this->object->$requested_property;
+        } else {
+            return $this->_null_value;
+        }
     }
-    else {
-      return $this->_null_value;
+
+    /**
+     * Implements the magic __set method.
+     *
+     * @param string $set_property
+     * @param mixed $value
+     */
+    public function __set($set_property, $value)
+    {
+        $this->object->$set_property = $value;
     }
-  }
 
-  /**
-   * Implements the magic __set method.
-   *
-   * @param string $set_property
-   * @param mixed $value
-   */
-  public function __set($set_property, $value) {
-    $this->object->$set_property = $value;
-  }
+    /**
+     * Implements the magic __isset method.
+     *
+     * @param string $property
+     */
+    public function __isset($property)
+    {
+        return ($this->__get($property) !== null);
+    }
 
-  /**
-   * Implements the magic __isset method.
-   *
-   * @param string $property
-   */
-  public function __isset($property) {
-    return ($this->__get($property) !== NULL);
-  }
+    /**
+     * Implements the magic __unset method.
+     *
+     * @param string $property
+     */
+    public function __unset($property)
+    {
+        unset($this->object->$property);
+    }
 
-  /**
-   * Implements the magic __unset method.
-   *
-   * @param string $property
-   */
-  public function __unset($property) {
-    unset($this->object->$property);
-  }
-
-  /**
-   * Helper function to load the object.
-   */
-  protected function load() {
-    $this->loaded = TRUE;
-  }
-
+    /**
+     * Helper function to load the object.
+     */
+    protected function load()
+    {
+        $this->loaded = true;
+    }
 }
--- src/CloakingDevice/Drupal/GroupNode.php
+++ src/CloakingDevice/Drupal/GroupNode.php
@@ -1,67 +1,64 @@
 <?php
-/**
- * Provides an on-demand object injector.
- *
- * @author pdrake
- *
- */
-
-Namespace CloakingDevice\Drupal;
+namespace CloakingDevice\Drupal;
 
 use CloakingDevice\Cloak as Cloak;
-use \stdClass;
+use stdClass;
 
-class GroupNode extends Cloak {
+/**
+ * Provides an on-demand group node object injector.
+ */
+class GroupNode extends Cloak
+{
 
-  /**
-   * Build the node object.
-   *
-   * @param integer $nid
-   */
-  public function __construct($nid) {
-    $this->object = new stdClass();
-    $this->object->nid = $nid;
-  }
-
-  /**
-   * Implementation of the magic __get method.
-   *
-   * @param string $requested_property
-   * @return mixed
-   */
-  public function &__get($requested_property) {
-    // If the property is not set and we aren't fully loaded, attempt to load
-    // the property directly or via node_load.
-    if (!isset($this->object->$requested_property) && !$this->loaded) {
-      switch ($requested_property) {
-        case 'og_private':
-          $sql = 'SELECT og_private FROM {og} WHERE nid = %d';
-          $properties = db_fetch_array(db_query($sql, $this->object->nid));
-          foreach ($properties as $property => $value) {
-            $this->object->$property = $value;
-          }
-          break;
-
-        default:
-          $this->load();
-        break;
-      }
+    /**
+     * Build the node object.
+     *
+     * @param integer $nid
+     */
+    public function __construct($nid)
+    {
+        $this->object = new stdClass();
+        $this->object->nid = $nid;
     }
 
-    if (isset($this->object->$requested_property)) {
-      return $this->object->$requested_property;
-    }
-    else {
-      return $this->_null_value;
-    }
-  }
+    /**
+     * Implementation of the magic __get method.
+     *
+     * @param string $requested_property
+     * @return mixed
+     */
+    public function &__get($requested_property)
+    {
+        // If the property is not set and we aren't fully loaded, attempt to load
+        // the property directly or via node_load.
+        if (!isset($this->object->$requested_property) && !$this->loaded) {
+            switch ($requested_property) {
+                case 'og_private':
+                    $sql = 'SELECT og_private FROM {og} WHERE nid = %d';
+                    $properties = db_fetch_array(db_query($sql, $this->object->nid));
+                    foreach ($properties as $property => $value) {
+                        $this->object->$property = $value;
+                    }
+                    break;
+                default:
+                    $this->load();
+                    break;
+            }
+        }
 
-  /**
-   * Helper function to load the node object.
-   */
-  protected function load() {
-    $this->object = node_load($this->object->nid);
-    $this->loaded = TRUE;
-  }
+        if (isset($this->object->$requested_property)) {
+            return $this->object->$requested_property;
+        } else {
+            return $this->_null_value;
+        }
+    }
 
+    /**
+     * Helper function to load the node object.
+     */
+    protected function load()
+    {
+        $this->object = node_load($this->object->nid);
+        $this->loaded = true;
+    }
 }
